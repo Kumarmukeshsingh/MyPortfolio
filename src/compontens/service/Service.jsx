d9b86911df2013ab394931b86699bcc86afbb9e0
mukesh singh <kumarmukeshsapna2001@gmail.com>
2025-06-23 00:16:21 +0530

    delete file

--- src/compontens/service/Service.jsx
+++ src/compontens/service/Service.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import Slide from "../Slide/Slide";
+
 function Servic() {
   return (
     <>
       <div>
-        <Slide />
         <h1> i am a servive part</h1>
       </div>
     </>
